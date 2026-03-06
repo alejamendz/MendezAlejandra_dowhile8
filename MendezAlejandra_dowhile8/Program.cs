9d764c9bbbfc80d67e72ed184189e98efc34401a
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-03-05 19:15:44 -0600

    programa creado

--- MendezAlejandra_dowhile8/Program.cs
+++ MendezAlejandra_dowhile8/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Escriba un programa que muestre la tabla de multiplicar del 5");
