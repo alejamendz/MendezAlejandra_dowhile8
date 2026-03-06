837b7b1eda1a930f346cf652826ab875fc30177a
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-03-05 19:33:55 -0600

    uso de while y do while

--- MendezAlejandra_dowhile8/Program.cs
+++ MendezAlejandra_dowhile8/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Escriba un programa que muestre la tabla de multiplicar del 5");
+
+int numero = 1;
+
+do
+{
+    Console.WriteLine("5 x " + numero + " = " + (5 * numero));
+    numero++;
+}
+while (numero <= 10);
